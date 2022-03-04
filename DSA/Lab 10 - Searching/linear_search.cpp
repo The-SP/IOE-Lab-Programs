146868353b6248bef91cab07278044dea41f9203
Suraj Pathak <surajpathak2057@gmail.com>
2022-03-04 16:23:45 +0545

    Fix function name

--- DSA/Lab 10 - Searching/linear_search.cpp
+++ DSA/Lab 10 - Searching/linear_search.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int n, int key) {
+int linearSearch(int arr[], int n, int key) {
     int i;
     for (int i = 0; i < n; i++)
         if (arr[i] == key)
@@ -13,7 +13,7 @@ int main() {
     int arr[] = {10, 20, 80, 30, 60, 50, 110, 100, 130, 170};
     int n = sizeof(arr) / sizeof(arr[0]);
     int key = 130;
-    int result = binarySearch(arr, n, key);
+    int result = linearSearch(arr, n, key);
     (result == -1)
         ? cout << "Element " << key << " not found"
         : cout << "Element " << key << " found at index " << result;
