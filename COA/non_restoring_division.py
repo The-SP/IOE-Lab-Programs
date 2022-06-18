0e03c70a26de97d7cd55f82843674da7f28a2a90
Suraj Pathak <surajpathak2057@gmail.com>
2022-06-18 22:12:15 +0545

    COA: Div

---
+++ COA/non_restoring_division.py
@@ -0,0 +1,41 @@
+# Division of Two Unsigned Integer Binary Numbers
+# Non-Restoring Division Algorithm
+
+from binary_addition import binary_adder
+from binary_subtraction import twos_complement
+
+
+def non_restoring_division(Q, M):
+    count = len(Q)  # no of bits in dividend Q
+    M = M.zfill(count + 1)
+    M_comp = twos_complement(M)
+    # initialize accumulator with zero
+    A = "0" * len(M)
+
+    for i in range(count):
+        sign_bit = A[0]  # check sign of A
+        # Left shift A, Q
+        A = A[1:] + Q[0]
+        Q = Q[1:]  # one bit is less, which is empty
+        if sign_bit == "1":
+            # A <- A + M
+            A, carry = binary_adder(A, M)
+        else:
+            # A <- A - M
+            A, carry = binary_adder(A, M_comp)
+
+        Q += "0" if A[0] == "1" else "1"
+
+    if A[0] == "1":  # if A is negative
+        A, carry = binary_adder(A, M)
+    return (Q, A)
+
+
+if __name__ == "__main__":
+    Q = input("Enter dividend: ")
+    M = input("Enter divisor: ")
+    if int(Q) < int(M):
+        quotient, remainder = "0" * len(Q), Q
+    else:
+        quotient, remainder = non_restoring_division(Q, M)
+    print(f"Quotient = {quotient}, Remainder = {remainder}")
---
+++ COA/restoring_division.py
@@ -0,0 +1,43 @@
+# Division of Two Unsigned Integer Binary Numbers
+# Restoring Division Algorithm
+
+from binary_addition import binary_adder
+from binary_subtraction import twos_complement
+
+
+def restoring_division(Q, M):
+    count = len(Q)  # no of bits in dividend Q
+    if len(M) <= count:
+        M = M.zfill(count + 1)
+    M_comp = twos_complement(M)
+    # initialize accumulator with zero
+    A = "0" * len(M)
+
+    for i in range(count):
+        # Left shift A, Q
+        A = A[1:] + Q[0]
+        Q = Q[1:]  # one bit is less, which is empty
+
+        # A <- A - M
+        A, carry = binary_adder(A, M_comp)
+
+        if A[0] == "1":
+            # Set Qo to 0
+            Q = Q + "0"
+            # A <- A + M (restore A)
+            A, carry = binary_adder(A, M)
+        else:
+            # Set Qo to 1
+            Q = Q + "1"
+
+    return (Q, A)
+
+
+if __name__ == "__main__":
+    Q = input("Enter dividend: ")
+    M = input("Enter divisor: ")
+    if int(Q) < int(M):
+        quotient, remainder = "0" * len(Q), Q
+    else:
+        quotient, remainder = restoring_division(Q, M)
+    print(f"Quotient = {quotient}, Remainder = {remainder}")
