73d771959dbe92759338be0d4bc4691d682a90de
Suraj Pathak <surajpathak2057@gmail.com>
2022-06-21 12:24:41 +0545

    Instru: 3

--- Instrumentation II/Lab 3/3_1.asm
+++ Instrumentation II/Lab 3/3_1.asm
@@ -3,28 +3,28 @@
 
         MVI A, 80H
         OUT 43H
-	    LXI H, 9000H            ;look-up table location
+        LXI H, 9000H            ;look-up table location
         MVI B, 0AH
 TABLE:  MOV A, M
-	    CALL DISP               ;call display subroutine
+        CALL DISP               ;call display subroutine
         CALL DELAY
-       	INX H
+        INX H
         DCR B
-	    JNZ TABLE
+        JNZ TABLE
         RST 5
 
 ;;  Following subroutine displays the content of accumulator in 7-segment display
 DISP:	MVI C, 08H              ;counter to transmit 8-bit
 L1:     RLC                     ;to pass each bit serially from MSB to LSB
-	    MOV E, A
+        MOV E, A
         OUT 41H                 ;to pass a bit via PB0
-	    MVI A, 01H
+        MVI A, 01H
         OUT 42H                 ;clock single on PC0(rise)
-	    DCR A
+        DCR A
         OUT 42H                 ;clock signal on PC0(fall)
-	    MOV A, E
+        MOV A, E
         DCR C
-	    JNZ L1
+        JNZ L1
         RET
 
 ;; Delay subroutine
--- Instrumentation II/Lab 3/3_2.asm
+++ Instrumentation II/Lab 3/3_2.asm
@@ -7,10 +7,10 @@ START:  MVI B, 05H
         LXI H, 9000H            ;look-up table location
 TABLE:  MOV A, M
         CALL DISP               ;call display subroutine
-	    CALL DELAY
-	    INX H
+        CALL DELAY
+        INX H
         DCR B                   ;jump until we get null
-	    JNZ TABLE
+        JNZ TABLE
         JMP START
         RST 5
 
@@ -18,15 +18,15 @@ TABLE:  MOV A, M
 ;;  Following subroutine displays the content of accumulator in 7-segment display
 DISP:	MVI C, 08H              ;counter to transmit 8-bit
 L1:     RLC                     ;to pass each bit serially from MSB to LSB
-	    MOV E, A
+        MOV E, A
         OUT 41H                 ;to pass a bit via PB0
-	    MVI A, 01H
+        MVI A, 01H
         OUT 42H                 ;clock single on PC0(rise)
-	    DCR A
+        DCR A
         OUT 42H                 ;clock signal on PC0(fall)
-	    MOV A, E
+        MOV A, E
         DCR C
-	    JNZ L1
+        JNZ L1
         RET
 
 ;; Delay subroutine
