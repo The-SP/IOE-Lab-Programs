43d41e7abb1aca8efeb5c75b1ba82138dc778e41
Suraj Pathak <surajpathak2057@gmail.com>
2022-07-03 23:34:45 +0545

    COA: Page

--- COA/page_replacement.py
+++ COA/page_replacement.py
@@ -12,7 +12,9 @@ def FIFO(pages, capacity):
             queue.append(page)
             # Increment page fault
             page_faults += 1
-    return page_faults
+    page_hits = len(pages) - page_faults
+    hit_ratio = page_hits / (page_hits + page_faults)
+    return hit_ratio
 
 
 def LRU(pages, capacity):
@@ -36,10 +38,51 @@ def LRU(pages, capacity):
             page_list.remove(page)
             # Apped it at last index
             page_list.append(page)
-    return page_faults
+    page_hits = len(pages) - page_faults
+    hit_ratio = page_hits / (page_hits + page_faults)
+    return hit_ratio
+
+
+def LFU(pages, capacity):
+    page_faults = 0
+    page_list = []
+    hash = {}  # To store the pair (page, frequency)
+
+    for page in pages:
+        # MISS
+        if page not in page_list:
+            if len(page_list) == capacity:
+                # Remove least frequently used element (first element)
+                hash.pop(page_list[0])
+                page_list.pop(0)
+            # Store current page
+            page_list.append(page)
+            hash[page] = 0
+            # Increment page fault
+            page_faults += 1
+
+        # HIT
+        else:
+            # Remove previous index of current page
+            page_list.remove(page)
+            # Apped it at last index
+            page_list.append(page)
+            # Increase frequency
+            hash[page] += 1
+
+        # Sort the pages based on their frequency (Insert current page)
+        i = len(page_list) - 2
+        while (hash[page_list[i]] > hash[page_list[i + 1]]) and i >= 0:
+            page_list[i], page_list[i + 1] = page_list[i + 1], page_list[i]
+            i -= 1
+
+    page_hits = len(pages) - page_faults
+    hit_ratio = page_hits / (page_hits + page_faults)
+    return hit_ratio
 
 
 if __name__ == "__main__":
-    pages = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2]
-    print("FIFO:", FIFO(pages, 4))
-    print("LRU:", LRU(pages, 4))
+    pages = [2, 3, 2, 1, 5, 2, 4, 5, 3, 2, 5, 2]
+    print("FIFO:", FIFO(pages, 3))
+    print("LRU:", LRU(pages, 3))
+    print("LFU:", LFU(pages, 3))
