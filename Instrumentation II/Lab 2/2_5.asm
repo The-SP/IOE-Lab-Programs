c3f30ef1c67b3ad2be4f3b9a3637588fcdd2e3d1
Suraj Pathak <surajpathak2057@gmail.com>
2022-07-15 17:37:28 +0545

    Instru

--- Instrumentation II/Lab 2/2_5.asm
+++ Instrumentation II/Lab 2/2_5.asm
@@ -1,11 +1,8 @@
 ; Lab 2, No 5
-
+        MVI A, COH  ; mode 2
+        OUT 83H
         MVI A, 09H  ; set PC4
         OUT 83H
-        MVI A, 0DH  ; set PC6
-        OUT 83H 
-        MVI A, F0H  ; mode 2
-        OUT 83H 
         
         MVI A, 0DH  ; initialize interrupts
         SIM         ; unmask RST 6.5
