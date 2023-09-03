a86f0f598b9cbb602144251369ace93ab1c780c7
Suraj Pathak <surajpathak2057@gmail.com>
2023-09-03 18:04:08 +0545

    Computer network project

--- DSAP/Lab 1/1_3.m
+++ DSAP/Lab 1/1_3.m
@@ -2,15 +2,15 @@ a=0;b=1;c=0,odd=0,even=0;
 res = [a, b];
 while (a+b) <= 10000
     c = a+b;
-    res = [res, c]
+    res = [res, c];
     if rem(c, 2) == 0
-        even = even + 1
+        even = even + 1;
     else
-        odd = odd + 1
+        odd = odd + 1;
     end
     a = b;
     b = c;
 end
 
-x = [1:1:length(res)]
-plot(x, res)
+x = [1:1:length(res)];
+plot(x, res);
--- DSAP/Lab 2/2c.m
+++ DSAP/Lab 2/2c.m
@@ -4,16 +4,16 @@ th=pi/3;
 C=2+3i;
 r=0;
 subplot(2,2,1);
-y=abs(C)*exp(r*t).*(cos(w0.*t+th)+i*sin(w0.*t+th));
+y=abs(C)*exp(r*t).*(cos(w0*t+th)+i*sin(w0*t+th));
 plot(t,y);
 
 r=-1;
 subplot(2,2,2);
-y=abs(C)*exp(r*t).*(cos(w0.*t+th)+i*sin(w0.*t+th));
+y=abs(C)*exp(r*t).*(cos(w0*t+th)+i*sin(w0*t+th));
 plot(t,y);
 
 
 r=1;
 subplot(2,2,3);
-y=abs(C)*exp(r*t).*(cos(w0.*t+th)+i*sin(w0.*t+th));
+y=abs(C)*exp(r*t).*(cos(w0*t+th)+i*sin(w0*t+th));
 plot(t,y);
